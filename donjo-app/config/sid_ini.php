befe7b81b4ec182db1639c96b12a9e876f4466e9
Asep Budiyana M <asepbudiyana12@gmail.com>
2026-06-22 22:16:30 +0700

    update feedback 22 juni

--- donjo-app/config/sid_ini.php
+++ donjo-app/config/sid_ini.php
@@ -108,7 +108,8 @@ if (is_file($extra_app_config)) {
  * Untuk menggunakan fitur ini, pastikan konfigurasi apache di server SID
  * mengizinkan penggunaan .htaccess
  */
-if (file_exists(FCPATH . '.htaccess') && ENVIRONMENT != 'development') {
+// if (file_exists(FCPATH . '.htaccess') && ENVIRONMENT != 'development') {
+if (ENVIRONMENT != 'development') {
     $config['index_page'] = '';
 }
 
--- resources/views/admin/auth/index.blade.php
+++ resources/views/admin/auth/index.blade.php
@@ -67,7 +67,7 @@
                             @yield('content')
 
                             <hr style="margin-top: 5px; margin-bottom: 5px;" />
-                            <div class="login-footer-bottom"><a href="https://github.com/OpenSID/OpenSID" target="_blank">OpenSID</a> v{{ AmbilVersi() }}</div>
+                            <div class="login-footer-bottom"><a href="https://app.simdesa.id/" target="_blank">Simdesa</a> v{{ AmbilVersi() }}</div>
                         </div>
                     </div>
                 </div>
